36b2427faaeb07cbdb2690f6340f37940309864c
Pramod Kumar <prmodraj609@gmail.com>
2025-08-07 19:59:54 +0530

    added backend config

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'https://streak-code-beta.vercel.app',
+    baseURL: 'https://streakcode-z6gn.onrender.com',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
