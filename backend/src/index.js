e0569febfaf04ed4501fe30fc368047c9aa42092
Pramod Kumar <prmodraj609@gmail.com>
2025-08-07 20:02:08 +0530

    added frontend config

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ const cors = require('cors')
 // console.log("Hello")
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://streak-code-beta.vercel.app',
     credentials: true 
 }))
 
